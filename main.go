286069e0ce6da38ddf70397279a060aee94f34f9
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-07-29 20:51:13 +0900

    add findTemplateDir

--- main.go
+++ main.go
@@ -8,15 +8,27 @@ import (
 	"path"
 )
 
+const (
+	TEMPLATE_FILE_EXAMPLE = "page.html"
+	REPOSITORY_DIR        = "syaro"
+)
+
 var (
 	wikiRoot    string
-	templateDir = "/home/yuntan/Workspace/go/src/syaro/templates/"
+	templateDir string
 )
 
 func main() {
 	// print welcome message
-	fmt.Println("=== syaro wiki server ===")
+	fmt.Println("===== Syaro Wiki Server =====")
 	fmt.Println("Starting...")
+
+	templateDir = findTemplateDir("")
+	if templateDir == "" {
+		fmt.Println("Error: Can't find template dir.")
+		return
+	}
+	fmt.Println("Template dir:", templateDir)
 
 	// set repository root
 	if len(os.Args) == 2 {
@@ -24,7 +36,7 @@ func main() {
 	} else {
 		wikiRoot = "./"
 	}
-	fmt.Println("WikiRoot:" + wikiRoot)
+	fmt.Println("WikiRoot:", wikiRoot)
 
 	fmt.Println("Server started. Waiting connection on port :8080")
 	fmt.Println()
@@ -34,6 +46,41 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+// findTemplateDir finds template directory contains html, css, etc...
+// dir is directory specified by user as template dir.
+// This search several directory and return right dir.
+// If not found, return empty string.
+func findTemplateDir(dir string) string {
+	// if template dir is specified by user, search this dir
+	if dir != "" {
+		_, err := os.Stat(path.Join(dir, TEMPLATE_FILE_EXAMPLE))
+		// if directory isn't exist
+		if err != nil {
+			fmt.Println("Error: Can't find template file dir specified in argument")
+			return ""
+		}
+		return dir
+	} else { // directory isn't specified by user so search it by myself
+		// first, $GOROOT/src/...
+		_, err := os.Stat(path.Join(os.Getenv("GOROOT"), "src", REPOSITORY_DIR,
+			"template", TEMPLATE_FILE_EXAMPLE))
+		if err == nil {
+			return path.Join(os.Getenv("GOROOT"), "src", REPOSITORY_DIR)
+		}
+
+		// second, /usr/local/share/syaro
+		_, err = os.Stat(path.Join("/usr/local/share/syaro", "templates",
+			TEMPLATE_FILE_EXAMPLE))
+		if err == nil {
+			return path.Join("/usr/local/share/syaro", "templates")
+		}
+
+		// can't find template dir
+		return ""
+	}
+}
+
+// handler is basic http request handler
 func handler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Printf("Response received (%s)\n", req.URL.Path)
 
@@ -61,11 +108,11 @@ func handler(rw http.ResponseWriter, req *http.Request) {
 		// render html
 		err = page.Render(rw)
 		if err != nil {
-			fmt.Println("rendering error!" + err.Error())
+			fmt.Println("rendering error!", err.Error())
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
 		}
 
-	} else if ext := path.Ext(filePath); ext == ".css" || ext == ".js" || ext == ".ico" {
+	} else if ext := path.Ext(filePath); ext == ".css" || ext == ".js" {
 		f, err := os.Open(path.Clean(templateDir + filePath))
 		if err != nil {
 			fmt.Println("not found")
