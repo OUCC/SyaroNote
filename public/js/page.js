5f7e362b2f550ac27c64039d3dc44cc2642aedc4
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2015-02-08 13:37:20 +0900

    [ADD] execute action (add, rename) by enter in modal input

--- public/js/page.js
+++ public/js/page.js
@@ -6,115 +6,26 @@ $(function(){
   }
 
   function initUI() {
-    $('.alert').hide()
-    $('.modal').hide()
+    $('.alert').hide();
+    $('.modal').hide();
 
-    $('#createModalInput').val(syaro.wikiPath)
-    $('#renameModalInput').val(syaro.wikiPath)
+    $('#createModalInput').val(syaro.wikiPath);
+    $('#renameModalInput').val(syaro.wikiPath);
 
-    $('#createModalButton').on('click', function() {
-      var name = $('#createModalInput').val()
-      if (name === "") {
-        $('#createErrorAlert').html('<strong>Error</strong> Please fill blank form.')
-        $('#createErrorAlert').show()
-        return
-      }
+    $('#createModalButton').on('click', createPage);
+    $('#createModalInput').keypress(function (event) {
+      if (event.keyCode === 13) { createPage(); } // enter
+    });
 
-      if (name[0] !== '/') { name = '/' + name }
-      var reqUrl = syaro.urlPrefix + encodeURIComponent(name).replace(/%2F/g, '/');
+    $('#renameModalButton').on('click', renamePage);
+    $('#renameModalInput').keypress(function (event) {
+      if (event.keyCode === 13) { renamePage(); }
+    });
 
-      var req = new XMLHttpRequest()
-      req.open('POST', reqUrl);
-
-      req.onreadystatechange = function() {
-        if (req.readyState === 4) {
-          $('#createModalButton').button('reset')
-
-          switch (req.status) {
-          case 201: // created
-            // redirect to editor
-            location.href = reqUrl + '?view=editor'
-            break
-
-          default:
-            // show error alert
-            $('#createErrorAlert').html('<strong>Error</strong> ' + req.responseText)
-            $('#createErrorAlert').show()
-            break
-          }
-        }
-      }
-
-      req.send()
-      $('#createModalButton').button('loading')
-    })
-
-    $('#renameModalButton').on('click', function() {
-      var name = $('#renameModalInput').val()
-      if (name === "") {
-        $('#renameErrorAlert').html('<strong>Error</strong> Please fill brank form.')
-        $('#renameErrorAlert').show()
-        return
-      }
-
-      if (name[0] !== '/') { name = '/' + name }
-      var reqUrl = syaro.urlPrefix + encodeURIComponent(name).replace(/%2F/g, '/');
-
-      var req = new XMLHttpRequest()
-      req.open('PUT', reqUrl + '?action=rename&oldpath='
-        + encodeURIComponent(syaro.wikiPath).replace(/%2F/g, '/'))
-
-      req.onreadystatechange = function() {
-        if (req.readyState === 4) {
-          $('#renameModalButton').button('reset')
-
-          switch (req.status) {
-          case 200: // ok
-            // redirect to page
-            location.href = reqUrl
-            break
-
-          default:
-            // show error alert
-            $('#renameErrorAlert').html('<strong>Error</strong> ' + req.responseText)
-            $('#renameErrorAlert').show()
-            break
-          }
-        }
-      }
-
-      req.send()
-      $('#renameModalButton').button('loading')
-    })
-
-    $('#deleteModalButton').on('click', function() {
-      var req = new XMLHttpRequest()
-      req.open('DELETE', location.href)
-
-      req.onreadystatechange = function() {
-        if (req.readyState === 4) {
-          $('#deleteModalButton').button('reset')
-
-          switch (req.status) {
-          case 200: // ok
-            location.reload(true)
-            break
-
-          default:
-            // show error alert
-            $('#deleteErrorAlert').html('<strong>Error</strong> ' + req.responseText)
-            $('#deleteErrorAlert').show()
-            break
-          }
-        }
-      }
-
-      req.send()
-      $('#deleteModalButton').button('loading')
-    })
+    $('#deleteModalButton').on('click', deletePage);
 
     nav = $('.syaro-main nav');
-    toggle = nav.children('.toc-toggle')
+    toggle = nav.children('.toc-toggle');
     toggle.on('click', function() {
       if(nav.hasClass('toc-open')) {
         nav.removeClass('toc-open');
@@ -135,6 +46,107 @@ $(function(){
     $(".markdown").each(function() {
       emojify.run($(this).get(0));
     });
+  }
+
+  function createPage() {
+    var name = $('#createModalInput').val()
+    if (name === "") {
+      $('#createErrorAlert').html('<strong>Error</strong> Please fill blank form.')
+      $('#createErrorAlert').show()
+      return
+    }
+
+    if (name[0] !== '/') { name = '/' + name }
+    var reqUrl = syaro.urlPrefix + encodeURIComponent(name).replace(/%2F/g, '/');
+
+    var req = new XMLHttpRequest()
+    req.open('POST', reqUrl);
+
+    req.onreadystatechange = function() {
+      if (req.readyState === 4) {
+        $('#createModalButton').button('reset')
+
+        switch (req.status) {
+        case 201: // created
+          // redirect to editor
+          location.href = reqUrl + '?view=editor'
+          break
+
+        default:
+          // show error alert
+          $('#createErrorAlert').html('<strong>Error</strong> ' + req.responseText)
+          $('#createErrorAlert').show()
+          break
+        }
+      }
+    }
+
+    req.send()
+    $('#createModalButton').button('loading')
+  }
+
+  function renamePage() {
+    var name = $('#renameModalInput').val()
+    if (name === "") {
+      $('#renameErrorAlert').html('<strong>Error</strong> Please fill brank form.')
+      $('#renameErrorAlert').show()
+      return
+    }
+
+    if (name[0] !== '/') { name = '/' + name }
+    var reqUrl = syaro.urlPrefix + encodeURIComponent(name).replace(/%2F/g, '/');
+
+    var req = new XMLHttpRequest()
+    req.open('PUT', reqUrl + '?action=rename&oldpath='
+      + encodeURIComponent(syaro.wikiPath).replace(/%2F/g, '/'))
+
+    req.onreadystatechange = function() {
+      if (req.readyState === 4) {
+        $('#renameModalButton').button('reset')
+
+        switch (req.status) {
+        case 200: // ok
+          // redirect to page
+          location.href = reqUrl
+          break
+
+        default:
+          // show error alert
+          $('#renameErrorAlert').html('<strong>Error</strong> ' + req.responseText)
+          $('#renameErrorAlert').show()
+          break
+        }
+      }
+    }
+
+    req.send()
+    $('#renameModalButton').button('loading')
+  }
+
+  function deletePage() {
+    var req = new XMLHttpRequest()
+    req.open('DELETE', location.href)
+
+    req.onreadystatechange = function() {
+      if (req.readyState === 4) {
+        $('#deleteModalButton').button('reset')
+
+        switch (req.status) {
+        case 200: // ok
+          location.reload(true)
+          break
+
+        default:
+          // show error alert
+          $('#deleteErrorAlert').html('<strong>Error</strong> ' + req.responseText)
+          $('#deleteErrorAlert').show()
+          break
+        }
+      }
+    }
+
+    req.send()
+    $('#deleteModalButton').button('loading')
   }
 
   init()
