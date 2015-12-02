69b0c8c94b70942bcc5f7db00ae8da58c801ce45
Yuto Tokunaga <yuntan.sub1@gmail.com>
2015-12-02 19:19:32 +0900

    Add a lot of functionary to page
    fix modal behavior
    fix create, rename, delete page funcs
    fix filelist style
    add filelist menus
    add functionary to rename, delete files
    add file uploader
    fix nav contents to right pane when user scroll page
    add history pane
    redesign page
    add meta data
    add title header
    show search box only when search option is enabled
    add jquery typing

--- public/js/page.js
+++ public/js/page.js
@@ -1,45 +1,282 @@
+/* global emojify */
+/* global syaro */
 $(function(){
+"use strict";
+
+  var historyLoaded = false,
+      reMarkdown = /\.(md|mkd|mkdn|mdown|markdown)$/,
+      commErrorText = "Communication error";
 
   function init() {
-    initUI();
+    initModal();
+    initFileList();
+    initFileListMenu();
     initEmojify();
+    initHistory();
+
+    if ($('.syaro-right nav')) {
+      $(window).scroll(scrollNav);
+    }
   }
 
-  function initUI() {
+  function initModal() {
     $('.alert').hide();
-    $('.modal').hide();
+    $('.modal').modal('hide');
 
-    $('#createModalInput').val(syaro.wikiPath);
-    $('#renameModalInput').val(syaro.wikiPath);
-
-    $('#createModalButton').on('click', createPage);
-    $('#createModalInput').keypress(function (event) {
-      if (event.keyCode === 13) { createPage(); } // enter
+    $('#createModal').on('show.bs.modal', function (ev) {
+      $('#createModal .alert').hide();
+      $('#createModal input').val('');
     });
 
-    $('#renameModalButton').on('click', renamePage);
-    $('#renameModalInput').keypress(function (event) {
-      if (event.keyCode === 13) { renamePage(); }
+    $('#createModal').on('shown.bs.modal', function (ev) {
+      $('#createModal input').focus();
     });
 
-    $('#deleteModalButton').on('click', deletePage);
+    $('#createModalButton').on('click', function (ev) {
+      var filename = $('#createModal input').val();
+      if (filename.length === 0) { return; }
 
-    nav = $('.syaro-main nav');
-    toggle = nav.children('.toc-toggle');
-    toggle.on('click', function() {
-      if(nav.hasClass('toc-open')) {
-        nav.removeClass('toc-open');
-        nav.addClass('toc-close');
-        toggle.children('i').removeClass('glyphicon-chevron-up');
-        toggle.children('i').addClass('glyphicon-chevron-down');
+      $('#createModalButton').button('loading')
+      if (!filename.match(reMarkdown)) {
+        filename = filename + '.md';
       }
-      else {
-        nav.removeClass('toc-close');
-        nav.addClass('toc-open');
-        toggle.children('i').removeClass('glyphicon-chevron-down');
-        toggle.children('i').addClass('glyphicon-chevron-up');
+      if (filename[0] !== '/') {
+        filename = syaro.wikiPath + '/' + filename;
+      }
+
+      Promise.resolve({
+        url: '/api/new?wpath=' + encodeURIComponent(filename),
+        method: 'GET',
+      }).then(xhr)
+        .then(function (arg) {
+          // redirect to editor
+          location.href = '/edit?wpath=' + encodeURIComponent(filename);
+        })
+        .catch(function (arg) {
+          $('#createModalButton').button('reset');
+          $('#createModal .alert').html('<strong>Error</strong> ' +
+            // (status === 302 ? "Already exists" : "Internal server error"));
+            (arg.responseText ? arg.responseText : commErrorText));
+          $('#createModal .alert').show();
+        });
+    });
+
+    $('#renameModal').on('show.bs.modal', function () {
+      $('#renameModal .alert').hide();
+
+      var src = $('input#modalData').val();
+      $('#renameModal .modal-title').text("Rename " + src);
+      $('#renameModal input').val(src);
+    });
+
+    $('#renameModal').on('shown.bs.modal', function () {
+      $('#renameModal input').focus();
+    });
+
+    $('#renameModalButton').on('click', function () {
+      var src = $('input#modalData').val();
+      var dst = $('#renameModal input').val();
+      if (dst.length === 0) { return; }
+      if (dst[0] !== '/') {
+        dst = syaro.wikiPath + '/' + dst;
+      }
+
+      $('#renameModalButton').button('loading')
+
+      renameFile(src, dst)
+      .then(function (status) {
+        // refreash Page
+        location.reload();
+      })
+      .catch(function (status) {
+        $('#renameModalButton').button('reset');
+        $('#renameModal .alert').html('<strong>Error</strong> ' +
+            (status === 404 ? "Not found" : "Internal server error"));
+        $('#renameModal .alert').show();
+      });
+    });
+
+    $('#deleteModal').on('show.bs.modal', function () {
+      $('#deleteModal .alert').hide();
+
+      var wpath = $('input#modalData').val();
+      $('#deleteModal .modal-title').text("Delete " + wpath);
+      var msg = "<p>Are you sure you want to <strong>DELETE</strong> " + wpath + "?";
+      if (syaro.isDir) {
+        msg += "<p>All contents under " + wpath + " will be deleted!</p>";
+      }
+      $('#deleteModal .msg').html(msg);
+    });
+
+    $('#deleteModalButton').on('click', function () {
+      var wpath = $('input#modalData').val();
+
+      $('#deleteModalButton').button('loading')
+
+      deleteFile(wpath)
+      .then(function (status) {
+        // refreash Page
+        location.reload();
+      })
+      .catch(function (status) {
+        $('#deleteModalButton').button('reset');
+        $('#deleteModal .alert').html('<strong>Error</strong> ' +
+            (status === 404 ? "Not found" : "Internal server error"));
+        $('#deleteModal .alert').show();
+      });
+    });
+  }
+
+  function initFileList() {
+    $('.uploader-wrapper').hide();
+    $('.uploader-wrapper .progress').hide();
+    $('.uploader-wrapper .alert').hide();
+
+    // select file list item by clicking and show extra menu
+    $('.syaro-filelist table tr').on('click', function (ev) {
+      var selected = $(this).hasClass('selected');
+      $('.syaro-filelist table tr.selected').removeClass('selected');
+      if (!selected) {
+        $(this).addClass('selected');
+      }
+
+      if ($('.syaro-filelist table tr.selected').length === 0) {
+        $('.panel-heading .menu-group li.extra').hide();
+      } else {
+        $('.panel-heading .menu-group li.extra').show();
       }
     });
+
+    $('.syaro-filelist table').on('dragover', function (ev) {
+      ev.preventDefault();
+      $('.uploader-wrapper').show();
+    });
+
+    $('.uploader-wrapper').on('dragover', function (ev) {
+      ev.preventDefault();
+      ev.stopPropagation();
+      $('.uploader-wrapper').show();
+      return false;
+    });
+
+    $('.uploader-wrapper').on('dragenter', function (ev) {
+      ev.preventDefault();
+      ev.stopPropagation();
+      return false;
+    });
+
+    $('.uploader-wrapper').on('drop', function (ev) {
+      ev.preventDefault();
+      $('.uploader-wrapper').show();
+      $('.uploader-wrapper .uploader-message').hide();
+      uploadMulti(ev.originalEvent.dataTransfer.files);
+    });
+
+    $('#uploadForm').on('change', function (ev) {
+      $('.uploader-wrapper').show();
+      $('.uploader-wrapper .uploader-message').hide();
+      uploadMulti(this.files);
+    });
+  }
+
+  function initFileListMenu() {
+    $('.panel-heading .menu-group li.extra').hide();
+
+    $('#createPage').on('click', function (ev) {
+      ev.preventDefault();
+      $('#createModal').modal('show');
+    });
+
+    $('#uploadFile').on('mouseover', function () {
+      $('.syaro-filelist .uploader-wrapper').show();
+    });
+
+    $('#uploadFile').on('mouseout', function () {
+      $('.syaro-filelist .uploader-wrapper').hide();
+    });
+
+    $('#uploadFile').on('click', function (ev) {
+      ev.preventDefault();
+      $('#uploadForm').trigger('click');
+    });
+
+    $('#renameFile').on('click', function (ev) {
+      ev.preventDefault();
+
+      var src = $('.syaro-filelist table tr.selected')[0].children[2].innerText;
+      $('input#modalData').val(src);
+
+      $('#renameModal').modal('show');
+    });
+
+    $('#deleteFile').on('click', function (ev) {
+      ev.preventDefault();
+
+      var wpath = $('.syaro-filelist table tr.selected')[0].children[2].innerText;
+      $('input#modalData').val(wpath);
+
+      $('#deleteModal').modal('show');
+    });
+  }
+
+  // http://www.html5rocks.com/ja/tutorials/file/dndfiles/
+  function uploadMulti(files) {
+    $('.uploader-wrapper .progress').show();
+
+    var l = files.length, c = 0;
+    var ps = [];
+    for (var i = 0; i < l; i++) {
+      var f = files[i];
+      var wpath = syaro.wikiPath + '/' + f.name;
+
+      ps.push(
+        Promise.resolve({wpath: wpath, file: f})
+          .then(readFile)
+          .then(uploadFile)
+          .then(function (arg) {
+            c++;
+            var now = 100*c/l;
+            $('.uploader-wrapper .progress-bar').css({width: now+'%'});
+            return arg;
+          })
+      );
+    }
+
+    Promise.all(ps)
+      .then(function (arg) {
+        location.reload();
+      })
+      .catch(function (err) {
+        $('.uploader-wrapper .progress').hide();
+        $('.uploader-wrapper .alert').html('<strong>Error</strong> ' + err.responseText);
+        $('.uploader-wrapper .alert').show();
+      });
+  }
+
+  function readFile(arg) {
+    return new Promise(function(resolve, reject) {
+      var r = new FileReader();
+      r.onloadend = function () {
+        resolve({wpath: arg.wpath, blob: r.result});
+      };
+      r.onerror = function () {
+        reject(new Error(r.error));
+      }
+      r.readAsBinaryString(arg.file);
+    });
+  }
+
+  function scrollNav() {
+    var nav = $('.syaro-right nav');
+    // get top offset including margin
+    // var navTop = nav.position().top + nav.parent().offset().top;
+    var navTop = nav.parent().offset().top;
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > navTop) {
+      nav.addClass('affix');
+    } else {
+      nav.removeClass('affix');
+    }
   }
 
   function initEmojify() {
@@ -53,105 +290,150 @@ $(function(){
     });
   }
 
-  function createPage() {
-    var name = $('#createModalInput').val()
-    if (name === "") {
-      $('#createErrorAlert').html('<strong>Error</strong> Please fill blank form.')
-      $('#createErrorAlert').show()
-      return
-    }
+  function initHistory() {
+    $('a[href="#syaro-history"]').on('shown.bs.tab', function () {
+      if (historyLoaded) { return; }
 
-    if (name[0] !== '/') { name = '/' + name }
-    var reqUrl = syaro.urlPrefix + encodeURIComponent(name).replace(/%2F/g, '/');
+      Promise.resolve({
+        url: '/api/history?wpath=' + encodeURIComponent(syaro.wikiPath),
+        method: 'GET',
+      }).then(xhr)
+        .then(function (arg) {
+          var data = JSON.parse(arg.response);
+          $('#syaro-history .progress').hide();
 
-    var req = new XMLHttpRequest()
-    req.open('POST', reqUrl);
+          var panel = '<div class="panel panel-info">' +
+            '<div class="panel-heading">History of ' +
+            syaro.wikiPath + '</div>'
+          var thead = '<thead><tr><th>Op</th><th>Message</th><th>Author</th><th>Date</th></tr></thead>';
+          var table = '<table class="table table-striped  table-bordered">' + thead + '<tbody>';
+          table += '<thead>'
+          table += data.map(function (c) {
+            return '<tr><td>' + [c.op, c.msg, c.name, c.date].join('</td><td>') +
+              '</td></tr>';
+          }).join('');
+          table += '</tbody></table>';
 
-    req.onreadystatechange = function() {
-      if (req.readyState === 4) {
-        $('#createModalButton').button('reset')
-
-        switch (req.status) {
-        case 201: // created
-          // redirect to editor
-          location.href = reqUrl + '?view=editor'
-          break
-
-        default:
-          // show error alert
-          $('#createErrorAlert').html('<strong>Error</strong> ' + req.responseText)
-          $('#createErrorAlert').show()
-          break
-        }
-      }
-    }
-
-    req.send()
-    $('#createModalButton').button('loading')
+          $('#syaro-history').html(panel + table + '</div>');
+          historyLoaded = true;
+        })
+        .catch(function (arg) {
+          $('#syaro-history .progress').hide();
+          $('#syaro-history .alert').html('<strong>Error</strong> ' +
+            arg.status + ' ' + (arg.responseText ? arg.responseText : arg.statusText));
+          $('#syaro-history .alert').show();
+          historyLoaded = true;
+        });
+    });
   }
 
-  function renamePage() {
-    var name = $('#renameModalInput').val()
-    if (name === "") {
-      $('#renameErrorAlert').html('<strong>Error</strong> Please fill brank form.')
-      $('#renameErrorAlert').show()
-      return
-    }
+  function createPage(filename) {
+    return new Promise(function (resolve, reject) {
+      var url = '/api/new?wpath=' + encodeURIComponent(filename);
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
 
-    if (name[0] !== '/') { name = '/' + name }
-    var reqUrl = syaro.urlPrefix + encodeURIComponent(name).replace(/%2F/g, '/');
-
-    var req = new XMLHttpRequest()
-    req.open('PUT', reqUrl + '?action=rename&oldpath='
-      + encodeURIComponent(syaro.wikiPath).replace(/%2F/g, '/'))
-
-    req.onreadystatechange = function() {
-      if (req.readyState === 4) {
-        $('#renameModalButton').button('reset')
-
-        switch (req.status) {
-        case 200: // ok
-          // redirect to page
-          location.href = reqUrl
-          break
-
-        default:
-          // show error alert
-          $('#renameErrorAlert').html('<strong>Error</strong> ' + req.responseText)
-          $('#renameErrorAlert').show()
-          break
+      req.onreadystatechange = function () {
+        if (req.readyState !== XMLHttpRequest.DONE) { return; }
+        if (req.status === 201) { // StatusCreated
+          resolve(req.status);
+        } else {
+          reject(req.status);
         }
-      }
-    }
+      };
 
-    req.send()
-    $('#renameModalButton').button('loading')
+      req.send();
+    });
   }
 
-  function deletePage() {
-    var req = new XMLHttpRequest()
-    req.open('DELETE', location.href)
+  function uploadFile(arg) {
+    return new Promise(function (resolve, reject) {
+      var url = '/api/upload?wpath=' + encodeURIComponent(arg.wpath);
+      var req = new XMLHttpRequest();
+      req.open('POST', url);
 
-    req.onreadystatechange = function() {
-      if (req.readyState === 4) {
-        $('#deleteModalButton').button('reset')
-
-        switch (req.status) {
-        case 200: // ok
-          location.reload(true)
-          break
-
-        default:
-          // show error alert
-          $('#deleteErrorAlert').html('<strong>Error</strong> ' + req.responseText)
-          $('#deleteErrorAlert').show()
-          break
+      req.onreadystatechange = function () {
+        if (req.readyState !== XMLHttpRequest.DONE) { return; }
+        if (req.status === 201) { // StatusCreated
+          resolve({wpath: arg.wpath});
+        } else {
+          reject({
+            status: req.status,
+            statusText: req.statusText,
+            responseText: req.responseText,
+          });
         }
-      }
-    }
+      };
 
-    req.send()
-    $('#deleteModalButton').button('loading')
+      req.send(arg.blob);
+    });
+  }
+
+  function renameFile(src, dst) {
+    return new Promise(function (resolve, reject) {
+      var url = '/api/rename?src=' + encodeURIComponent(src) + ';dst=' + encodeURIComponent(dst);
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+
+      req.onreadystatechange = function () {
+        if (req.readyState !== XMLHttpRequest.DONE) { return; }
+        if (req.status === 200) { // StatusOK
+          resolve(req.status);
+        } else {
+          reject(req.status);
+        }
+      };
+
+      req.send();
+    })
+  }
+
+  function deleteFile(wpath) {
+    return new Promise(function (resolve, reject) {
+      var url = '/api/delete?wpath=' + encodeURIComponent(wpath);
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+
+      req.onreadystatechange = function () {
+        if (req.readyState !== XMLHttpRequest.DONE) { return; }
+        if (req.status === 200) { // StatusOK
+          resolve(req.status);
+        } else {
+          reject(req.status);
+        }
+      };
+
+      req.send();
+    })
+  }
+
+  function xhr(arg) {
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open(arg.method, arg.url);
+
+      req.onreadystatechange = function () {
+        if (req.readyState !== XMLHttpRequest.DONE) { return; }
+
+        var arg = $.extend(arg, {
+          status: req.status,
+          statusText: req.statusText,
+          response: req.response,
+          responseText: req.responseText,
+        });
+        if (req.status/100 === 2) {
+          resolve(arg);
+        } else {
+          reject(arg);
+        }
+      };
+
+      req.onerror = function () {
+        reject(arg);
+      }
+
+      req.send();
+    });
   }
 
   init()
